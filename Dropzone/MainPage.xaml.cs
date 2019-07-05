a7ee1a402b5fdb3d40b9cdf5685b0ae1b246e85b
ian gaul <iangaul@pdx.edu>
2019-07-05 13:35:24 -0700

    welcome message and test counter

--- Dropzone/MainPage.xaml.cs
+++ Dropzone/MainPage.xaml.cs
@@ -17,5 +17,11 @@ namespace Dropzone
         {
             InitializeComponent();
         }
+        int count = 0;
+        void Button_Clicked(object sender, System.EventArgs e)
+        {
+            count++;
+            ((Button)sender).Text = $"You clicked {count} times.";
+        }
     }
 }
