cff69e8546842ca000330dd7e8c9317499fe6395
Yufei <taoyufeih@me.com>
2019-07-16 13:17:11 -0700

    Added content for AddJumpPage, still considering which button layout to use

---
+++ Dropzone.Android/obj/Release/android/typemap.jm.arm64-v8a.s
@@ -0,0 +1,26 @@
+	/* Data SHA1: 38957b79a96610dda7f86e6abec6788bcd449b3e */
+	.arch	armv8-a
+	.file	"typemap.jm.inc"
+
+	/* Mapping header */
+	.section	.data.jm_typemap,"aw",@progbits
+	.type	jm_typemap_header, @object
+	.p2align	2
+	.global	jm_typemap_header
+jm_typemap_header:
+	/* version */
+	.word	1
+	/* entry-count */
+	.word	6526
+	/* entry-length */
+	.word	266
+	/* value-offset */
+	.word	121
+	.size	jm_typemap_header, 16
+
+	/* Mapping data */
+	.type	jm_typemap, @object
+	.global	jm_typemap
+jm_typemap:
+	.size	jm_typemap, 1735917
+	.include	"typemap.jm.inc"
---
+++ Dropzone.Android/obj/Release/android/typemap.mj.arm64-v8a.s
@@ -0,0 +1,26 @@
+	/* Data SHA1: 842663bbe2907367d5eb16dd31f8e1bddad185d4 */
+	.arch	armv8-a
+	.file	"typemap.mj.inc"
+
+	/* Mapping header */
+	.section	.data.mj_typemap,"aw",@progbits
+	.type	mj_typemap_header, @object
+	.p2align	2
+	.global	mj_typemap_header
+mj_typemap_header:
+	/* version */
+	.word	1
+	/* entry-count */
+	.word	7495
+	/* entry-length */
+	.word	266
+	/* value-offset */
+	.word	145
+	.size	mj_typemap_header, 16
+
+	/* Mapping data */
+	.type	mj_typemap, @object
+	.global	mj_typemap
+mj_typemap:
+	.size	mj_typemap, 1993671
+	.include	"typemap.mj.inc"
